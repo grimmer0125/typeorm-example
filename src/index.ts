5e923d478ce268f011f0e98c414e8ee3b1e0bfb2
Grimmer <grimmer0125@gmail.com>
2020-05-14 23:58:55 +0800

    Refactor index.ts and add User.ts to test Active Record pattern

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,51 @@
-import {createConnection} from "typeorm";
-import {Post} from "./entity/Post";
-import {Category} from "./entity/Category";
+import { createConnection, Connection } from "typeorm";
+import { Post } from "./entity/Post";
+import { Category } from "./entity/Category";
+import { User } from "./entity/User";
+
+async function dataMapper(connection: Connection) {
+  const category1 = new Category();
+  category1.name = "TypeScript";
+  await connection.manager.save(category1);
+
+  const category2 = new Category();
+  category2.name = "Programming";
+  await connection.manager.save(category2);
+
+  const post = new Post();
+  post.title = "Control flow based type analysis";
+  post.text = `TypeScript 2.0 implements a control flow-based type analysis for local variables and parameters.`;
+  post.categories = [category1, category2];
+
+  await connection.manager.save(post);
+
+  console.log("Post has been saved: ", post);
+}
+
+async function activeRecord(connectin: Connection) {
+  const user = new User();
+  user.firstName = "Timber";
+  user.lastName = "Saw";
+  user.age = 25;
+  await user.save();
+
+  const allUsers = await User.find();
+  const firstUser = await User.findOne(1);
+  const timber = await User.findOne({ firstName: "Timber", lastName: "Saw" });
+
+  await timber.remove();
+}
+
+(async () => {
+  try {
+    const connectin = await createConnection();
+    // await dataMapper(connectin);
+    await activeRecord(connectin);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+})();
 
 // connection settings are in the "ormconfig.json" file
-createConnection().then(async connection => {
-
-    const category1 = new Category();
-    category1.name = "TypeScript";
-    await connection.manager.save(category1);
-
-    const category2 = new Category();
-    category2.name = "Programming";
-    await connection.manager.save(category2);
-
-    const post = new Post();
-    post.title = "Control flow based type analysis";
-    post.text = `TypeScript 2.0 implements a control flow-based type analysis for local variables and parameters.`;
-    post.categories = [category1, category2];
-
-    await connection.manager.save(post);
-
-    console.log("Post has been saved: ", post);
-
-}).catch(error => console.log("Error: ", error));
+//   .then(async (connection) => {})
+//   .catch((error) => console.log("Error: ", error));
